71608cedec4adaca8f45a9df7640749b9c446eb6
nghiaduong2202 <nghia.duong2202@gmail.com>
2025-07-24 16:22:07 +0700

    add backend init

---
+++ backend/src/main/java/com/nghiaduong/LMS_Project/LmsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.nghiaduong.LMS_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsProjectApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/nghiaduong/LMS_Project/LmsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nghiaduong.LMS_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
